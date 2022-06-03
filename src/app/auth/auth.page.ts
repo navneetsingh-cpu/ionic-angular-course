5aa167a79e887d829e9a96313d944b8c86fac702
Navneet <hunnyhunnyhunny@gmail.com>
2022-06-03 12:08:34 +0530

    Added Edit offer and new offer page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
@@ -11,6 +12,8 @@ import { AuthService } from './auth.service';
 export class AuthPage implements OnInit {
 
   isLoading = false;
+  isLogin = true;
+
   constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() { }
@@ -36,5 +39,24 @@ export class AuthPage implements OnInit {
 
     const { role, data } = await loading.onDidDismiss();
     console.log('Loading dismissed!');
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -7,6 +7,8 @@ export interface Place {
   description: string;
   imageUrl: string;
   price: number;
+  availableTo: Date;
+  availableFrom: Date;
 }
 
 @Injectable({
@@ -24,13 +26,17 @@ export class PlacesService {
         title: 'Manhattan Mansion',
         description: 'In the heart of New York City.',
         imageUrl: 'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
-        price: 149.99
+        price: 149.99,
+        availableFrom: new Date('2022-01-01'),
+        availableTo: new Date(),
       }, {
         id: 'p2',
         title: `L'Amour Toujours`,
         description: 'A romantic place in Paris!',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
-        price: 189.99
+        price: 189.99,
+        availableFrom: new Date('2022-01-01'),
+        availableTo: new Date(),
       },
 
       {
@@ -38,7 +44,9 @@ export class PlacesService {
         title: 'The Foggy Palace',
         description: 'Not your average city trip!',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
-        price: 99.99
+        price: 99.99,
+        availableFrom: new Date('2022-01-01'),
+        availableTo: new Date(),
       }
     ];
   }
